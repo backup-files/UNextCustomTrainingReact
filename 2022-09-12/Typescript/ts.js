f9ee2ea822659ff4cd8576c94a96fcbc2f6f6de1
tezz-io <tejasmr@outlook.com>
2022-09-12 16:41:36 +0530

    Typescript activity done

---
+++ 2022-09-12/Typescript/ts.js
@@ -0,0 +1,5 @@
+var username = 'john';
+var age = 25;
+var userMarried = true;
+var salary = 200000000;
+console.log(username, age, userMarried, salary);
---
+++ 2022-09-12/Typescript/ts.ts
@@ -0,0 +1,6 @@
+var username: string = 'john';
+var age:number = 25;
+var userMarried: boolean = true;
+var salary: number = 200_000_000; 
+
+console.log(username, age, userMarried, salary);
