8f35f3b4bdeb32da1d01b6bd240a5063f9f7f9cd
tezz-io <tejasmr@outlook.com>
2022-09-12 14:11:44 +0530

    Method overloading activity done

---
+++ 2022-09-12/MethodOverloading/mo.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a+b;
+}
+function add(a, b, c) {
+    return a*b;
+}
+
+// const add = (a, b) => a+b;
+// const add = (a, b, c) => a*b; 
+
+console.log(add(10, 20));
